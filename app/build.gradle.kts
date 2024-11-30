9ab453dc65bb3b42a9af0ea65e9cef845d660d0f
Dangerwind <dangerwind@gmail.com>
2024-11-30 13:00:09 +0300

    add chackstile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,11 @@ repositories {
 }
 
 dependencies {
+    implementation("info.picocli:picocli:4.7.1")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
 }
 
 tasks.test {
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,31 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.util.concurrent.Callable;
+
+@Command(name="gendiff", mixinStandardHelpOptions = true, version = "gendiff V1.0b",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+    @CommandLine.Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+    @CommandLine.Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+    @CommandLine.Option(
+            names = {"-f", "--format"},
+            paramLabel = "format",
+            defaultValue = "stylish",
+            description = "output format [default: stylish]"
+    )
+    private String format;
+
+    @Override
+    public Integer call() throws Exception {
+        return null;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
