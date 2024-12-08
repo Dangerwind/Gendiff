8c3f02fc83a101ccc82675ff52087be9635cfc56
Dangerwind <dangerwind@gmail.com>
2024-12-08 15:06:44 +0300

    fix style

--- app/src/test/java/AppTests.java
+++ app/src/test/java/AppTests.java
@@ -10,41 +10,41 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import hexlet.code.Differ;
 
 public class AppTests {
-    private static String CompareStylish;
-    private static String CompareJson;
-    private static String ComparePlain;
+    private static String compareStylish;
+    private static String compareJson;
+    private static String comparePlain;
 
-    private static final String resultStylish = "src/test/resources/fixtures/result.stylish";
-    private static final String resultJson = "src/test/resources/fixtures/result.json";
-    private static final String resultPlain = "src/test/resources/fixtures/result.plain";
-    private static final String pathToFixtures = "src/test/resources/fixtures/file";
+    private static final String FILE_STYLISH = "src/test/resources/fixtures/result.stylish";
+    private static final String FILE_JSON = "src/test/resources/fixtures/result.json";
+    private static final String FILE_PLAIN = "src/test/resources/fixtures/result.plain";
+    private static final String PATH_FIXTURES = "src/test/resources/fixtures/file";
 
 
     @BeforeAll
     public static void readAllAnswers() throws Exception {
-       CompareStylish = new String(Files.readAllBytes(Paths.get(resultStylish)));
-       CompareJson = new String(Files.readAllBytes(Paths.get(resultJson)));
-       ComparePlain = new String(Files.readAllBytes(Paths.get(resultPlain)));
+        compareStylish = new String(Files.readAllBytes(Paths.get(FILE_STYLISH)));
+        compareJson = new String(Files.readAllBytes(Paths.get(FILE_JSON)));
+        comparePlain = new String(Files.readAllBytes(Paths.get(FILE_PLAIN)));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"json", "yaml"})
     public void testStylishResult(String format) throws Exception {
-        String actual = Differ.generate(pathToFixtures + "1." + format, pathToFixtures + "2." + format, "stylish");
-        assertEquals(CompareStylish, actual);
+        String actual = Differ.generate(PATH_FIXTURES + "1." + format, PATH_FIXTURES + "2." + format, "stylish");
+        assertEquals(compareStylish, actual);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"json", "yaml"})
     public void testPlainResult(String format) throws Exception {
-        String actual = Differ.generate(pathToFixtures + "1." + format, pathToFixtures + "2." + format, "plain");
-        assertEquals(ComparePlain, actual);
+        String actual = Differ.generate(PATH_FIXTURES + "1." + format, PATH_FIXTURES + "2." + format, "plain");
+        assertEquals(comparePlain, actual);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"json", "yaml"})
     public void testJsonResult(String format) throws Exception {
-        String actual = Differ.generate(pathToFixtures + "1." + format, pathToFixtures + "2." + format, "json");
-        assertEquals(CompareJson, actual);
+        String actual = Differ.generate(PATH_FIXTURES + "1." + format, PATH_FIXTURES + "2." + format, "json");
+        assertEquals(compareJson, actual);
     }
 }
